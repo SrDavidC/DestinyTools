fba9a0c9ac2e586b8f1632f302202048d5d042ef
DavidC <david.cerdasalvarado@ucr.ac.cr>
2023-05-23 15:58:14 -0600

    fix: Cana de pescar have some issues with their durability

---
+++ src/main/java/me/srdqrk/destinytools/items/strategies/CanaDePescarStrategy.java
@@ -0,0 +1,32 @@
+package me.srdqrk.destinytools.items.strategies;
+
+import me.srdqrk.destinytools.DestinyTools;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+
+
+import static me.srdqrk.destinytools.items.ItemsManager.getUsesLeft;
+import static me.srdqrk.destinytools.items.ItemsManager.setUsesLeft;
+
+public class CanaDePescarStrategy implements IItemStrategy {
+  @Override
+  public void execute(PlayerInteractEvent event, ItemStack item) {
+    int usesLeft = getUsesLeft(item);
+    if (usesLeft > 0) {
+      usesLeft--;
+      item = setUsesLeft(item, usesLeft);
+      Damageable meta = (Damageable) item.getItemMeta();
+      meta.setDamage(meta.getDamage() + (64/15));
+      item.setItemMeta(meta);
+      event.getPlayer().getInventory().setItemInMainHand(item);
+    } else {
+      event.setCancelled(true);
+      Location loc = event.getPlayer().getLocation();
+      loc.getWorld().playSound(loc, Sound.ENTITY_ITEM_BREAK, 1f, 1f);
+      event.getPlayer().sendActionBar(DestinyTools.instance().getMm().deserialize("<white> La caña se ha roto"));
+    }
+  }
+}
